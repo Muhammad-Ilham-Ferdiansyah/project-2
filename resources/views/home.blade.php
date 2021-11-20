b52ef750b308dcb0d2869801388f93cd517d49cb
Muhammad-Ilham-Ferdiansyah <ilhamferdiansyah737@gmail.com>
2021-11-20 20:50:37 +0700

    Menambahkan views landing page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.main')
+
+@section('container')
+
+
+    <div class="container">
+
+        <div class="row mt-5 mb-5">
+
+            <div class="col-lg-12 gambar">
+                <img src="https://res.cloudinary.com/dffrqml8w/image/upload/v1637413471/vektor-mahasiswa_avsfon.png"
+                    width="100%">
+            </div>
+
+            <div class="col-sm-12 position-relative p-4">
+
+                <div class="position-absolute top-0 end-0">
+                    <img src="https://res.cloudinary.com/dffrqml8w/image/upload/v1637413471/vektor-mahasiswa_avsfon.png"
+                        class="img">
+                </div>
+
+                <h1 class="display-1 tebel-sedang">Sistem Informasi <br> Pertukaran Mahasiswa <br> APERTI BUMN <br>
+                    Politeknik Pos Indonesia</h1>
+
+                <div class="desc mt-4">
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                        sed diam nonummy nibh euismod tincidunt ut laoreet
+                        dolore magna </p>
+                </div>
+
+                <div class="tombol mt-5">
+                    <a href="#" class="button rounded-pill shadow tebel-sedang">REGISTER</a>
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </div>
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-light mt-4">
+    <div class="container">
+        <a class="navbar-brand m-3" href="#">
+            SIPMA BUMN
+        </a>
+        <button class="navbar-toggler mb-4" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a class="nav-link link tebel-sedang" href="/">Home &nbsp;&nbsp;</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link tebel-sedang" href="#">About &nbsp;&nbsp;</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link tebel-sedang" href="#">Services &nbsp;&nbsp;</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link tebel-sedang" href="#">Contact &nbsp;&nbsp;</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active tebel-sedang rounded-pill bg-ungu shadow" href="#">Login</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
